16903ae7dc112869709c5b1b9f6cd188212d3c56
luongnguyenminhan <annd11@fpt.com>
2026-01-13 17:21:15 +0700

    update API endpoint and brand name in env-config

--- public/env-config.js
+++ public/env-config.js
@@ -1,7 +1,7 @@
 // public/env-config.js
 window.__ENV__ = {
-    API_ENDPOINT: 'https://securescribe.wc504.io.vn/be/api',
-    BRAND_NAME: 'Meobeo.ai',
+    API_ENDPOINT: 'https://chang.wc504.io.vn/be/api',
+    BRAND_NAME: 'Chang',
     BRAND_LOGO: 'https://mom.meobeo.ai/images/logos/logo.png',
     FCM_VAPID_KEY: 'BLgY0pHLGwzER2db0ebbNX2950NiidC6il--p-RS0d7Q5WdIbNktz9MjQsCO4O1XpjznZghy0PzjXIb026qoX9I' // Add your FCM VAPID key here
 };
